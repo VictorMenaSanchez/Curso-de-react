579f50a292f18f6f36d3d579b53f40178426a5b6
victor <vmenasanchez@gmail.com>
2025-04-08 11:41:18 +0200

    Añadidas funcionalidades de JSX

--- segunda-aplicacion/src/App.js
+++ segunda-aplicacion/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  //Definir el estado del componente para almacenar el resultado de la suma
+
+  const [resultado, setResultado] = useState(null);
+  const elemento = <h1 className="centrar-titulo">Hola Mundo</h1>;
+  const elemento2 = <h2> {suma(5 ,7)}</h2>;
+
+  const botonPulsado = () => {
+    //Declaro el estado del componente y lo actualizo con setResultado
+    const result = suma(5 ,7);
+    setResultado(result);
+    
+  }
+
+  return <div>
+    <button onClick={botonPulsado} style={{marginTop:  "10px", marginLeft: "10px"}}>Púlsame</button>
+    <div>{elemento}</div>
+    <div>{resultado != null &&<h2>El resultado es: {resultado}</h2>}</div>
     </div>
-  );
+  
+}
+/*
+function botonPulsado(){
+  alert("Botón pulsado");
+}
+*/
+/*
+const botonPulsado = () => {
+  alert(suma(5 ,7));
+}
+  */
+function suma(a , b){
+  return a + b;
 }
 
 export default App;
