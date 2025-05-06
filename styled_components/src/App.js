c9b7223d70edd4cc8fe5deb9b1f7b811c03e46b6
victor <vmenasanchez@gmail.com>
2025-05-06 10:59:35 +0200

    Styled components

---
+++ styled_components/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { MiCaja } from './components/styled';
+
+function App() {
+  return (
+    <div className="App">
+      <MiCaja>
+        Entrar
+      </MiCaja>
+    </div>
+  );
+}
+
+export default App;
